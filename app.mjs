ecc918df17a1ea583d0101c7da1b7d4383424251
muhrafifrbn <afifrabbani04@gmail.com>
2024-10-18 01:29:41 +0700

    menampilkan data mahasiswa berdasarkan nim

--- app.mjs
+++ app.mjs
@@ -9,6 +9,7 @@ app.get("/", (req, res) => {
   res.send("SIMPLE CRUD Mahasiswa");
 });
 
+// Menampilkan semua data mahasiswa
 app.get("/mahasiswa", (req, res) => {
   const sql = "SELECT * FROM tester";
   db.query(sql, (error, result) => {
@@ -17,6 +18,20 @@ app.get("/mahasiswa", (req, res) => {
       response(200, result, "Show Data", res);
     } catch (error) {
       response(500, error.sqlMessage, "Error", res);
+    }
+  });
+});
+
+// menampilakan data mahasiswa berdasarkan nim
+app.get("/mahasiswa/:nim", (req, res) => {
+  const sql = `SELECT * FROM tester WHERE nim = ${req.params.nim}`;
+  const message = "display data base on nim";
+  db.query(sql, (error, result) => {
+    try {
+      if (error) throw error;
+      response(200, result, message, res);
+    } catch (error) {
+      response(500, error.sqlMessage, message, res);
     }
   });
 });
